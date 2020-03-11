25b456f1d83da406f4abbda2c40e7a348af1dd43
hexingyu <hxy@cx5y.cn>
2020-03-11 14:34:02 +0800

    更新vue的版本

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,26 @@
   <div>
     <img src="./logo.png">
     <h1>Hello Vue 3!</h1>
-    <button @click="inc">Clicked {{ count }} times.</button>
+    <m-button @click="inc">Clicked {{ count }} times.</m-button>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
+import MButton from './MButton'
+
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 export default {
-  setup () {
+  components: { MButton },
+  setup (props, { emit }) {
     const count = ref(0)
-    const inc = () => {
+    const inc = async () => {
       count.value++
+      await sleep(1000)
+      return 11
     }
     return {
       count,
